55cb97c6c3f785ec779827ac4752fce1c4c82b47
CHANCH10 <chester.chan@oocl.com>
2024-11-18 16:41:15 +0800

    fix: fix the output error

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -3,15 +3,48 @@ package com.tw;
 public class MultiplicationTableBuilder {
 
     public static void main(String[] args) {
-        MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
-        int start = 2;
-        int end = 4;
-        String multiplicationTable = builder.build(start, end);
-
-        System.out.println(multiplicationTable);
+        System.out.println(printMultiplicationTable(2,4));
+        //        printMultiplicationTable(2, 4);
     }
 
-    public String build(int start, int end) {
-        return "";
+
+    public static boolean isStartNumSmallerThanOrEqualToEndNum(int startNumber, int endNumber) {
+        return startNumber <= endNumber;
     }
+
+
+
+    public static Boolean isStartAndEndNumInRange(int startNumber, int endNumber) {
+        if (startNumber >= 0 && endNumber >= 0) {
+            return true;
+        } else {
+            return null;
+        }
+    }
+
+
+
+    public static String printMultiplicationTable(int startNumber, int endNumber) {
+        String equationsInAString = "" ;
+        for (int i = startNumber; i <= endNumber; i++) {
+
+
+
+            for (int j = startNumber; j <= i ;  j++ ) {
+                equationsInAString = equationsInAString + " " +  (j + "*" + i + "=" + (i * j));
+                if (j == i) {
+                    equationsInAString = equationsInAString + "\n";
+                }
+            }
+
+        }
+        return equationsInAString;
+    }
+
+
+
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
 }
