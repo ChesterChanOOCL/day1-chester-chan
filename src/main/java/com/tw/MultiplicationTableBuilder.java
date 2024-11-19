3fbd8fe8183efef0ede351dfbbe987557d592853
CHANCH10 <chester.chan@oocl.com>
2024-11-19 09:39:39 +0800

    fix: fix after code review on - changing build() check condition syntax - change isStartAndEndNumInRange check condition

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -7,7 +7,6 @@ public class MultiplicationTableBuilder {
 
     public static void main(String[] args) {
         System.out.println(build(2,4));
-        //        printMultiplicationTable(2, 4);
     }
 
 
@@ -16,50 +15,20 @@ public class MultiplicationTableBuilder {
     }
 
 
-
+    // Also Check the case of startnum and endNum exceeding the range
     public static Boolean isStartAndEndNumInRange(int startNumber, int endNumber) {
-        if (startNumber > 1 && endNumber < 1000) {
+        if (startNumber > 1 && startNumber <= 1000 && endNumber > 1  && endNumber <= 1000 ) {
             return true;
         } else {
             return false;
         }
     }
-
-
-
-//    public static String build(int startNumber, int endNumber) {
-//        String equationsInAString = "" ;
-//        if (isStartNumSmallerThanOrEqualToEndNum(startNumber, endNumber)== false){
-//            return null;
-//        }
-//        if (isStartAndEndNumInRange(startNumber, endNumber)== false){
-//            return null;
-//        }
-//        for (int i = startNumber; i <= endNumber; i++) {
-//
-//
-//
-//            for (int j = startNumber; j <= i ;  j++ ) {
-//                equationsInAString = equationsInAString  +  (j + "*" + i + "=" + (i * j));
-//                if (j == i) {
-//                    equationsInAString = equationsInAString + "\n";
-//                }
-//                else {
-//                    equationsInAString = equationsInAString + " ";
-//                }
-//            }
-//
-//        }
-//        return equationsInAString;
-//    }
 public static String build(int startNumber, int endNumber) {
-    if (!isStartNumSmallerThanOrEqualToEndNum(startNumber, endNumber)) {
-        return null;
-    }
-    if (!isStartAndEndNumInRange(startNumber, endNumber)) {
+    if (!isStartNumSmallerThanOrEqualToEndNum(startNumber, endNumber) || !isStartAndEndNumInRange(startNumber, endNumber)) {
         return null;
     }
 
+// use multiply function
     return IntStream.rangeClosed(startNumber, endNumber)
             .mapToObj(i -> IntStream.rangeClosed(startNumber, i)
                     .mapToObj(j -> j + "*" + i + "=" + (i * j))
